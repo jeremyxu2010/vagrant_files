7078cbe6ee4017c779506ea4d5052278f9bbe003
Jeremy Xu <jeremyxu2010@gmail.com>
2019-03-31 23:26:01 +0800

    简化命令的写法

--- k8s-sshuttle-vpn/setup_sshuttle_vpn.sh
+++ k8s-sshuttle-vpn/setup_sshuttle_vpn.sh
@@ -2,9 +2,7 @@
 yum install -y sshuttle
 
 sleep 5
-sshuttle -r root@192.168.33.10 10.96.0.0/12 > sshuttle_svc_cidr.log 2>&1 &
-sleep 5
-sshuttle -r root@192.168.33.10 10.244.0.0/16 > sshuttle_pod_cidr.log 2>&1 &
+sshuttle -r root@192.168.33.10 10.96.0.0/12 10.244.0.0/16 > sshuttle.log 2>&1 &
 
 # test
 # kubectl get svc --all-namespaces
